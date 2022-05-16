f6efc755d9e912b4c38ab48bbba6b1566cfe2e09
agate <alaaeddine.zammel@esprit.tn>
2022-05-16 16:49:39 +0200

    Adding behaviour for add method && test succeeded

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,8 +1,42 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 public class Calculator {
 
     public static int add(String numbersAsString) {
-        if (numbersAsString.isEmpty())
-            return 0;
-        else return 1;
+        if (!numbersAsString.isEmpty()) {
+            List<Integer> numbers = strArrToIntList(getSplit(numbersAsString));
+            negativesNumbers(numbers);
+            return numbers.stream().reduce(Integer::sum).orElseThrow();
+        }
+        return 0;
+    }
+
+    private static void negativesNumbers(List<Integer> numbers) {
+        StringBuilder stringBuilder = new StringBuilder();
+        numbers.stream().filter(x -> x < 0).forEach(x -> stringBuilder.append(x).append(" "));
+        if (!stringBuilder.toString().isEmpty()) {
+            throw new RuntimeException("negative numbers are not supported" + stringBuilder.toString());
+        }
+    }
+
+    private static List<Integer> strArrToIntList(String[] strings) {
+        return Arrays.stream(strings).map(Integer::parseInt).collect(Collectors.toList());
+    }
+
+    private static String[] getSplit(String input) {
+        if (input.startsWith("//")) {
+            Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(input);
+            if (matcher.matches()) {
+                String delimiter = matcher.group(1);
+                String toSplit = matcher.group(2);
+                return toSplit.split(delimiter);
+            }
+            throw new RuntimeException("delimiter not supported");
+        }
+        return input.split(",|\n");
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -17,18 +17,34 @@ public class CalculatorTest {
 
     @Test
     void should_handle_new_lines() {
-        Assertions.assertEquals(Calculator.add("1\\n2,3"), 6);
+        Assertions.assertEquals(Calculator.add("1,2\n3"), 6);
 
     }
 
     @Test
     void should_return_sum_when_many_delimiters() {
-        Assertions.assertEquals(Calculator.add("//;\\n1;2"), 3);
+        Assertions.assertEquals(Calculator.add("//;\n1;2"), 3);
     }
 
     @Test
     void should_throw_exception_when_input_contains_negative() {
-        Assertions.assertEquals(Calculator.add("//;\\n1;-12"), -12);
+
+        RuntimeException thrown = Assertions.assertThrows(
+                RuntimeException.class,
+                () -> Calculator.add("1,-2,-4,-9,9"),
+                "negative numbers are not supported"
+        );
+
+    }
+
+    @Test
+    void should_throw_exception_when_delimiter_not_supported() {
+
+        RuntimeException thrown = Assertions.assertThrows(
+                RuntimeException.class,
+                () -> Calculator.add("1,\n2"),
+                "delimiter not supported"
+        );
 
     }
 }
