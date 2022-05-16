7506c24a33ed1de3394ab54edc9a9a6e675b1f2d
agate <alaaeddine.zammel@esprit.tn>
2022-05-16 15:52:37 +0200

    Adding behaviour for add method && tests fails

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,6 +1,8 @@
 public class Calculator {
 
-    public static int add(String numbersAsString){
-        return 0;
+    public static int add(String numbersAsString) {
+        if (numbersAsString.isEmpty())
+            return 0;
+        else return 1;
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -11,18 +11,24 @@ public class CalculatorTest {
 
     @Test
     void should_handle_many_numbers() {
-        Assertions.assertEquals(Calculator.add(""), 0);
+        Assertions.assertEquals(Calculator.add("1,2,3"), 6);
+
+    }
+
+    @Test
+    void should_handle_new_lines() {
+        Assertions.assertEquals(Calculator.add("1\\n2,3"), 6);
 
     }
 
     @Test
     void should_return_sum_when_many_delimiters() {
-        Assertions.assertEquals(Calculator.add(""), 0);
+        Assertions.assertEquals(Calculator.add("//;\\n1;2"), 3);
     }
 
     @Test
     void should_throw_exception_when_input_contains_negative() {
-        Assertions.assertEquals(Calculator.add(""), 0);
+        Assertions.assertEquals(Calculator.add("//;\\n1;-12"), -12);
 
     }
 }
